4bccc8634865a8b80f24d01f4531814eb36cdaeb
tinzi <tinzi@tinzi.ch>
2018-06-05 23:24:59 +0200

    implemented bonus options

--- solution-assignment1/app.js
+++ solution-assignment1/app.js
@@ -10,28 +10,45 @@ function LunchCheckController($scope) {
   const ENJOY_MSG = "Enjoy!";
   const TOO_MUCH_MSG = "Too much!";
   const EMPTY_LIST_MSG = "Please enter data first";
+  const GREEN_MESSAGE_CLASS = "message-green";
+  const RED_MESSAGE_CLASS = "message-red";
+  const GREEN_BORDER_CLASS = "border-green";
+  const RED_BORDER_CLASS = "border-red";
+  const HEALTHY_NUM_ITEMS = 3;
 
   $scope.lunchItemList = "";
   $scope.feedbackMessage = EMPTY_LIST_MSG;
+  $scope.messageClass = RED_MESSAGE_CLASS;
+  $scope.borderColor = RED_BORDER_CLASS;
 
   $scope.checkItems = function () {
-    if ($scope.lunchItemList == "") {
+    var numItems = countItems($scope.lunchItemList);
+    if (numItems == 0) {
       $scope.feedbackMessage = EMPTY_LIST_MSG;
+      $scope.messageClass = RED_MESSAGE_CLASS;
+      $scope.borderColor = RED_BORDER_CLASS;
     }
     else {
-      var numItems = countItems($scope.lunchItemList);
-      if (numItems > 3) {
+      $scope.messageClass = GREEN_MESSAGE_CLASS;
+      $scope.borderColor = GREEN_BORDER_CLASS;
+      if (numItems > HEALTHY_NUM_ITEMS) {
         $scope.feedbackMessage = TOO_MUCH_MSG;
       }
       else {
-          $scope.feedbackMessage = ENJOY_MSG;
+        $scope.feedbackMessage = ENJOY_MSG;
       }
     }
   };
 
 function countItems(itemString) {
   var itemArray = itemString.split(',');
-  return itemArray.length;
+  var numItems = itemArray.length;
+  for (var i = 0; i < itemArray.length; i++ ) {
+    if (itemArray[i].trim() == "") {
+      numItems = numItems - 1;
+    }
+  }
+  return numItems;
 }
 
 };
